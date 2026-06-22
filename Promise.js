2e9ae141540ea9afe2b452c10930ef3a4e97aea0
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-06-22 09:24:45 +0530

    Create Promise.js

---
+++ Promise.js
@@ -0,0 +1,13 @@
+const myPromise = new Promise((resolve, reject) => {
+    let success = true;
+
+    if (success) {
+        resolve("Promise resolved successfully!");
+    } else {
+        reject("Promise rejected!");
+    }
+});
+
+myPromise
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
