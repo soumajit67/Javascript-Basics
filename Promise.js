cabf2ddd5789139455b5b3bd55d4b3d28efb7852
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-06-22 09:28:24 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,45 +1,62 @@
 // Basic Promise 
 
-const myPromise = new Promise((resolve, reject) => {
-    let success = true;
-
-    if (success) {
-        resolve("Promise resolved successfully!");
-    } else {
-        reject("Promise rejected!");
-    }
-});
-
-myPromise
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
-
-
-
-// Promise with setTimeout
-
-const delay = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve("Data loaded after 3 seconds");
-    }, 3000);
-});
-
-delay.then(data => console.log(data));
+            const myPromise = new Promise((resolve, reject) => {
+                let success = true;
+            
+                if (success) {
+                    resolve("Promise resolved successfully!");
+                } else {
+                    reject("Promise rejected!");
+                }
+            });
+            
+            myPromise
+                .then(result => console.log(result))
+                .catch(error => console.log(error));
+            
+            
+            
+            // Promise with setTimeout
+            
+            const delay = new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve("Data loaded after 3 seconds");
+                }, 3000);
+            });
+            
+            delay.then(data => console.log(data));
 
 
 
 // Promise Returning Random Success or Failure
 
-const checkNumber = new Promise((resolve, reject) => {
-    const number = Math.random();
+            const checkNumber = new Promise((resolve, reject) => {
+                const number = Math.random();
+            
+                if (number > 0.5) {
+                    resolve(`Success! Number is ${number}`);
+                } else {
+                    reject(`Failed! Number is ${number}`);
+                }
+            });
+            
+            checkNumber
+                .then(result => console.log(result))
+                .catch(error => console.log(error));
 
-    if (number > 0.5) {
-        resolve(`Success! Number is ${number}`);
-    } else {
-        reject(`Failed! Number is ${number}`);
-    }
-});
 
-checkNumber
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+// Promise with Function
+
+            function divide(a, b) {
+                return new Promise((resolve, reject) => {
+                    if (b === 0) {
+                        reject("Cannot divide by zero");
+                    } else {
+                        resolve(a / b);
+                    }
+                });
+            }
+            
+            divide(20, 5)
+                .then(result => console.log("Result:", result))
+                .catch(error => console.log(error));
